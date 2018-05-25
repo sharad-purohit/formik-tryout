19b0f9e839a8a63d3fd3d722d6f83a03dfd52474
Sharad Purohit <sharadpurohit777@gmail.com>
2018-05-25 18:41:06 -0500

    Added dependencies

---
+++ src/CustomerForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Formik } from 'formik';
+
+class CustomerForm extends React.Component {
+
+    state = {
+
+    }
+
+    render() { 
+        return <div> My Form </div>
+    }
+}
